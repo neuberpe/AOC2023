c4174146ea2894526040fff990bdb9530323faa1
neuberpe <36083955+neuberpe@users.noreply.github.com>
2023-12-01 12:42:07 +0100

    Add files via upload

---
+++ AOC1_2023.py
@@ -0,0 +1,38 @@
+#Erste und Letzte Zahl finden, daraus 2.stellige Zahl bilden, Summe dieser ist Ergebnis
+import time
+start_time = time.time()
+
+with open("AOC1.txt", "r") as file:
+    data = file.read().replace("\n", ",")
+mylist = data.split(",") #transormieren des obigen strings in liste, gesplittet nach ","
+num ="0123456789" #was sind zahlen
+words=["one","two","three","four","five","six","seven","eight","nine"]
+
+res =[] #ergebins
+for k in mylist:
+	value =[] #zwischenergebnis
+	for i in k:	
+		if i in num: #nummern check
+			value.append(int(i))
+	res.append(value[0]*10+value[-1]) #mal 10 der ersten stelle plus einer stelle
+
+print("Ergebnis Teil 1: "+str(sum(res)))
+
+res=[]
+for k in mylist:
+	pos=0
+	value =[]
+	for i in k:
+		if i in num:
+			value.append(int(i))
+		elif len(k[pos:]) >= 2:
+			for m in words:
+				if m in k[pos:pos+len(m)]:
+					value.append(words.index(m)+1)
+		else:
+			pass
+		pos +=1
+	res.append(value[0]*10+value[-1])
+
+print("Ergebnis Teil 2: "+str(sum(res)))
+print("--- %s seconds ---" % (time.time() - start_time))
